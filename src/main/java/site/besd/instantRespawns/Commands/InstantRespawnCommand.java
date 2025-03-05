d980d0d3834e389a6b4dea2fe12e2582b3ce08ee
besddev <besdinemail@gmail.com>
2025-03-05 23:03:48 +0200

    added tab completions

--- src/main/java/site/besd/instantRespawns/Commands/InstantRespawnCommand.java
+++ src/main/java/site/besd/instantRespawns/Commands/InstantRespawnCommand.java
@@ -5,10 +5,16 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class InstantRespawnCommand implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class InstantRespawnCommand implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (args.length == 0) {
@@ -31,5 +37,17 @@ public class InstantRespawnCommand implements CommandExecutor {
         }
 
         return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+
+        final List<String> validArguments = new ArrayList<>();
+
+        if (args.length == 1) {
+            StringUtil.copyPartialMatches(args[0], List.of("reload", "toggle", "disable", "enable"), validArguments);
+            return validArguments;
+        }
+        return List.of();
     }
 }
